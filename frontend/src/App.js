82335ff9dfd2dc9513b25416f4383cbd312a0ed7
Rafael Santos <rafaelnsantos1@gmail.com>
2019-04-04 19:46:25 -0300

    add i18n

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,26 +10,31 @@ import {
 import { MainNavigation, Notification } from './components';
 import './App.css';
 import GAListener from './GAListener';
+import { I18nextProvider } from 'react-i18next';
+
+import i18n from './i18n/i18n';
 
 const App = props => {
   return (
-    <BrowserRouter>
-      <GAListener>
-        <NotificationProvider>
-          <AuthProvider>
-            <ApolloProvider>
-              <GraphQLProvider>
-                <MainNavigation />
-                <main className="main-content">
-                  <Routes />
-                </main>
-                <Notification />
-              </GraphQLProvider>
-            </ApolloProvider>
-          </AuthProvider>
-        </NotificationProvider>
-      </GAListener>
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <GAListener>
+          <NotificationProvider>
+            <AuthProvider>
+              <ApolloProvider>
+                <GraphQLProvider>
+                  <MainNavigation />
+                  <main className="main-content">
+                    <Routes />
+                  </main>
+                  <Notification />
+                </GraphQLProvider>
+              </ApolloProvider>
+            </AuthProvider>
+          </NotificationProvider>
+        </GAListener>
+      </BrowserRouter>
+    </I18nextProvider>
   );
 };
 
---
+++ frontend/src/i18n/i18n.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import resources from './resources/';
+
+i18n.use(LanguageDetector).init({
+  resources,
+  fallbackLng: 'en-US',
+  debug: true,
+
+  // have a common namespace used around the full app
+  ns: ['commom', 'navigation', 'auth', 'error'],
+  defaultNS: 'commom',
+
+  keySeparator: false, // we use content as keys
+
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+    formatSeparator: ','
+  },
+
+  react: {
+    wait: true
+  }
+});
+
+export default i18n;
