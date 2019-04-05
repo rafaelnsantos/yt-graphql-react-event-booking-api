145a35f2dbc614c17f3c064c9bfe97f6f115eb46
Rafael Santos <rafaelnsantos1@gmail.com>
2019-04-04 22:00:23 -0300

    add es-ES

--- frontend/src/i18n/resources/index.js
+++ frontend/src/i18n/resources/index.js
@@ -1,7 +1,9 @@
 import enUS from './en-US.json';
 import ptBR from './pt-BR.json';
+import esES from './es-ES.json';
 
 export default {
   'en-US': enUS,
-  'pt-BR': ptBR
+  'pt-BR': ptBR,
+  'es-ES': esES
 };
